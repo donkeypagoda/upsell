51ef68b07d11ce618c1047b8e2fafa4a22d5ae3d
donkeypagoda <christopherallenstewart@gmail.com>
2017-06-29 15:54:59 -0700

    prices fixed

--- js/bikes.js
+++ js/bikes.js
@@ -4,84 +4,84 @@ let bikes = [
   brand: "Badass Bikes",
   gears: "24 speed",
   frame_size: "700cm",
-  price: "350",
+  price: 350,
   description: "Everyone will love you and give you free money & drugs if you buy this bike."
   },{
   img: "http://lorempixel.com/400/200/",
   brand: "Psycho Psycles",
   gears: "fixy",
   frame_size: "4in",
-  price: "5768",
+  price: 5768,
   description: "Have no fear, a rad bike is here."
   },{
   img: "http://lorempixel.com/400/200/",
   brand: "Wheels & Stuff",
   gears: "3.7gears",
   frame_size: "footlong",
-  price: "12",
+  price: 12,
   description: "The wheels on the bike go round and round, round and round."
   },{
   img: "http://lorempixel.com/400/200/",
   brand: "Hot Wheels",
   gears: "2πr²",
   frame_size: "itsy bitsy",
-  price: "3,458",
+  price: 3458,
   description: "I've got a bike. You can ride it if you like. It's got a basket, a bell that rings and Things to make it look good. I'd give it to you if I could, but I borrowed it."
   },{
   img: "http://lorempixel.com/400/200/",
   brand: "Badass Bikes",
   gears: "24 speed",
   frame_size: "700cm",
-  price: "350",
+  price: 350,
   description: "Everyone will love you and give you free money & drugs if you buy this bike."
   },{
   img: "http://lorempixel.com/400/200/",
   brand: "Psycho Psycles",
   gears: "fixy",
   frame_size: "4in",
-  price: "5768",
+  price: 5768,
   description: "Have no fear, a rad bike is here."
   },{
   img: "http://lorempixel.com/400/200/",
   brand: "Wheels & Stuff",
   gears: "3.7gears",
   frame_size: "footlong",
-  price: "12",
+  price: 12,
   description: "The wheels on the bike go round and round, round and round."
   },{
   img: "http://lorempixel.com/400/200/",
   brand: "Hot Wheels",
   gears: "2πr²",
   frame_size: "itsy bitsy",
-  price: "3,458",
+  price: 3458,
   description: "I've got a bike. You can ride it if you like. It's got a basket, a bell that rings and Things to make it look good. I'd give it to you if I could, but I borrowed it."
   },{
   img: "http://lorempixel.com/400/200/",
   brand: "Badass Bikes",
   gears: "24 speed",
   frame_size: "700cm",
-  price: "350",
+  price: 350,
   description: "Everyone will love you and give you free money & drugs if you buy this bike."
   },{
   img: "http://lorempixel.com/400/200/",
   brand: "Psycho Psycles",
   gears: "fixy",
   frame_size: "4in",
-  price: "5768",
+  price: 5768,
   description: "Have no fear, a rad bike is here."
   },{
   img: "http://lorempixel.com/400/200/",
   brand: "Wheels & Stuff",
   gears: "3.7gears",
   frame_size: "footlong",
-  price: "12",
+  price: 12,
   description: "The wheels on the bike go round and round, round and round."
   },{
   img: "http://lorempixel.com/400/200/",
   brand: "Hot Wheels",
   gears: "2πr²",
   frame_size: "itsy bitsy",
-  price: "3,458",
+  price: 3458,
   description: "I've got a bike. You can ride it if you like. It's got a basket, a bell that rings and Things to make it look good. I'd give it to you if I could, but I borrowed it."
   }
 ];
