0c1a155a984b031037708be140ce279b88ed8197
Heather Arvidson <heather.arvidson@gmail.com>
2017-06-29 12:14:14 -0700

    carousel forwards and backwards

--- js/index.js
+++ js/index.js
@@ -2,14 +2,45 @@
 
 //carousel
 $(document).ready(function(){
-  $('.carousel').carousel();
+  $('.carousel').carousel({ fullWidth: true, duration: 300, indicators: true});
+  $('.carousel').carousel('next');
+  $('.carousel').carousel('prev');
 });
 
- $('.carousel.carousel-slider').carousel({fullWidth: true});
- $('.carousel').carousel('next');
- $('.carousel').carousel('next', 3); // Move next n times.
- // Previous slide
- $('.carousel').carousel('prev');
- $('.carousel').carousel('prev', 4); // Move prev n times.
- // Set to nth slide
- $('.carousel').carousel('set', 4);
+let timer;
+
+autoplay()
+function autoplay() {
+    $('.carousel').carousel('next');
+    timer = setTimeout(autoplay, 5000);
+    console.log("autoplay invoked");
+}
+
+$('.carousel').on('mousedown', function(event) {
+  clearTimeout(timer);
+  console.log("timer reset");
+  console.log(event);
+});
+
+$('.carousel').on('mouseup', function(event) {
+  timer = setTimeout(autoplay, 5000);
+  console.log("timer restarted");
+  console.log(event);
+})
+
+// $('.carousel').on('wheel', function() {
+//   timer = setTimeout(autoplay, 5000);
+//   console.log("wheel event");
+// });
+
+//  $('.carousel').carousel('next', 3); // Move next n times.
+//  // Previous slide
+//  $('.carousel').carousel('prev', 4); // Move prev n times.
+//  // Set to nth slide
+//  $('.carousel').carousel('set', 4);
+
+
+ // $(document).ready(function(){
+ //   $('.carousel').carousel({indicators: true, dist: 0, shift: 100, padding: 1, noWrap: true,});
+ //   $('.carousel').carousel('set', 1);
+ // });
