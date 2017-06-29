9f6efeedc40f199cabc177d554cd66f7770b0d99
donkeypagoda <christopherallenstewart@gmail.com>
2017-06-28 21:46:22 -0700

    made some bikes in product array object

--- js/products.js
+++ js/products.js
@@ -1,1 +1,26 @@
 // products.js
+// image, brand, gears, frame-size, price
+let prodObjArray = [
+  {
+  img: "http://lorempixel.com/400/200/",
+  brand: "Badass Bikes",
+  gears: "24 speed",
+  frame_size: "700cm",
+  price: "£350",
+  description: "Everyone will love you and give you free money & drugs if you buy this bike."
+  },{
+  img: "http://lorempixel.com/400/200/",
+  brand: "Psycho Psycles",
+  gears: "fixy",
+  frame_size: "4in",
+  price: "¥5768",
+  description: "Have no fear, a rad bike is here."
+  },{
+  img: "http://lorempixel.com/400/200/",
+  brand: "Wheels & Stuff",
+  gears: "3.7gears",
+  frame_size: "footlong",
+  price: "¢12",
+  description: "The wheels on the bike go round and round, round and round."
+  }
+];
