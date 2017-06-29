d4919947c85d84f60fe92a8b1cce100164f8930e
donkeypagoda <christopherallenstewart@gmail.com>
2017-06-28 22:28:32 -0700

    monkeying around with some structure and JS for product page

--- js/products.js
+++ js/products.js
@@ -22,5 +22,25 @@ let prodObjArray = [
   frame_size: "footlong",
   price: "¢12",
   description: "The wheels on the bike go round and round, round and round."
-  }
+},{
+  img: "http://lorempixel.com/400/200/",
+  brand: "Hot Wheels",
+  gears: "2πr²",
+  frame_size: "itsy bitsy",
+  price: "€3,458",
+  description: "I've got a bike. You can ride it if you like. It's got a basket, a bell that rings and Things to make it look good. I'd give it to you if I could, but I borrowed it. You're the kind of girl that fits in with my world."
+}
 ];
+
+// maybe this is a bad idea below, maybe it shouldn't be totally dynamic, maybe we should just do 10-12 bikes
+// and have a fixed inventory at that?
+
+// function renderProducts(prodObjArray){
+//   const bikeCase = $("#bikeCase");
+//   for(const bike in bikes){
+//     let bikeRow = $("<div>").attr("class", "row");
+//     let bikeDiv = $("<div>").attr("class", "col s12 m6 l3");
+//     let bikeCard = $("<div>").attr("class", "card");
+//     let bikeImg
+//   }
+// }
